ef58288d2ffa65aa8d1492b9e11ab29fb437344a
joalon <joakimlonnegren@gmail.om>
2019-12-17 20:35:27 -0300

    Solves day 5, part 2

--- day5/day5.py
+++ day5/day5.py
@@ -1,105 +1,158 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-## Evaluation
 def evaluateIntcode(stack):
-    res = stepIntcode(stack, 0)
-    while res[0][res[1]] != 99:
-      res = stepIntcode(res[0], res[1])
-    print("Ran until HALT")
-    return res
+  newpointer = 0
+  newstack = stack.copy()
+  while newstack[newpointer] != 99:
+    newstack, newpointer = stepIntcode(newstack, newpointer)
+  print("Ran until HALT")
+  return newstack
 
 def stepIntcode(stack, stackpointer):
-    instruction, mode = getInstructionMode(stack[stackpointer])
-#    print("Executing: " + str(instruction) + " " + str(stack[stackpointer:stackpointer+4]) + " in mode " + str(mode))
-    result = operations[instruction][0](stack, stackpointer, mode)
-    return (result, stackpointer + operations[instruction][1])
+  instruction, mode = getInstructionMode(stack[stackpointer])
+  result, newpointer = operations[instruction](stack, stackpointer, mode)
+  return result, newpointer
 
-def getInstructionMode(instruction):
-    return int(str(instruction)[len(str(instruction))-2:]), [int(x) for x in str(instruction)[0:len(str(instruction))-2]]
+def getInstructionMode(instruction: int):
+  instructionString = str(instruction)
+  actualInstruction = int(instructionString[len(instructionString)-2:])
+  mode = instructionString[0:len(instructionString)-2].zfill(3)
+  mode = list(reversed([int(x) for x in mode]))
+  return actualInstruction, mode
 
 def intAdd(stack, pointer, mode):
-    result = stack.copy()
-    
-    operand1, operand2 = None, None
-    if not mode:
-        operand1 = result[result[pointer+1]]
-        operand2 = result[result[pointer+2]]
-    else:
-        if len(mode) == 1:
-            if mode[0] == 1:
-                operand1 = result[pointer+1]
-            else:
-                operand1 = result[result[pointer+1]]
-            operand2 = result[result[pointer+2]]
-        else:
-            if mode[1] == 1:
-                operand1 = result[pointer+1]
-            else:
-                operand1 = result[result[pointer+1]]
+  result = stack.copy()
+  
+  value1, value2 = None, None
+  if mode[0] == 0:
+    value1 = result[result[pointer+1]]
+  elif mode[0] == 1:
+    value1 = result[pointer+1]
 
-            if mode[0] == 1:
-                operand2 = result[pointer+2] 
-            else:
-                operand2 = result[result[pointer+2]]
+  if mode[1] == 0:
+    value2 = result[result[pointer+2]]
+  elif mode[1] == 1:
+    value2 = result[pointer+2]
 
-    result[result[pointer+3]] = operand1 + operand2
-    return result
+  result[result[pointer+3]] = value1 + value2
+  return result, pointer+4
 
 def intMultiply(stack, pointer, mode):
-    result = stack.copy()
+  result = stack.copy()
 
-    operand1, operand2 = None, None
-    if not mode:
-        operand1 = result[result[pointer+1]]
-        operand2 = result[result[pointer+2]]
-    else:
-        if len(mode) == 1:
-            if mode[0] == 1:
-                operand1 = result[pointer+1]
-            else:
-                operand1 = result[result[pointer+1]]
-            operand2 = result[result[pointer+2]]
-        else:
-            if mode[1] == 1:
-                operand1 = result[pointer+1]
-            else:
-                operand1 = result[result[pointer+1]]
+  value1, value2 = None, None
+  if mode[0] == 0:
+    value1 = result[result[pointer+1]]
+  elif mode[0] == 1:
+    value1 = result[pointer+1]
 
-            if mode[0] == 1:
-                operand2 = result[pointer+2] 
-            else:
-                operand2 = result[result[pointer+2]]
-
-    result[result[pointer+3]] = operand1 * operand2
-    return result
+  if mode[1] == 0:
+    value2 = result[result[pointer+2]]
+  elif mode[1] == 1:
+    value2 = result[pointer+2]
+  result[result[pointer+3]] = value1 * value2
+  return result, pointer+4
 
 def intInput(stack, pointer, mode):
     result = stack.copy()
-    inp = int(input("input: "))
-    result[result[pointer+1]] = inp
-    return result
+    result[result[pointer+1]] = int(input("input: "))
+    return result, pointer+2
 
 def intOutput(stack, pointer, mode):
-
-    output = 0
-    if not mode:
-      output = stack[stack[pointer+1]]
-    else:
+    output = None
+    if mode[0] == 1:
       output = stack[pointer+1]
+    elif mode[0] == 0:
+      output = stack[stack[pointer+1]]
 
     if output:
       print("DEBUG | " + str(output))
     else:
       print("TEST | " + str(output))
-    return stack
+    return stack, pointer+2
 
-operations = {
-    1: (intAdd, 4),
-    2: (intMultiply, 4),
-    3: (intInput, 2),
-    4: (intOutput, 2)
-}
+def intJumpIfTrue(stack, pointer, mode):
+  nextpointer = pointer + 3
+
+  value1, value2 = None, None
+  if mode[0] == 0:
+    value1 = stack[stack[pointer+1]]
+  elif mode[0] == 1:
+    value1 = stack[pointer+1]
+
+  if mode[1] == 0:
+    value2 = stack[stack[pointer+2]]
+  elif mode[1] == 1:
+    value2 = stack[pointer+2]
+
+  if value1 != 0:
+    nextpointer = value2
+
+  return stack, nextpointer
+
+def intJumpIfFalse(stack, pointer, mode):
+  nextpointer = pointer + 3
+
+  value1, value2= None, None
+  if mode[0] == 0:
+    value1 = stack[stack[pointer+1]]
+  elif mode[0] == 1:
+    value1 = stack[pointer+1]
+
+  if mode[1] == 0:
+    value2 = stack[stack[pointer+2]]
+  elif mode[1] == 1:
+    value2 = stack[pointer+2]
+
+  if value1 == 0:
+    nextpointer = value2
+
+  return stack, nextpointer
+
+def intLessthan(stack, pointer, mode):
+  result = stack.copy()
+
+  value1, value2 = None, None
+  if mode[0] == 0:
+    value1 = result[result[pointer+1]]
+  elif mode[0] == 1:
+    value1 = result[pointer+1]
+
+  if mode[1] == 0:
+    value2 = result[result[pointer+2]]
+  elif mode[1] == 1:
+    value2 = result[pointer+2]
+
+  result[result[pointer+3]] = 1 if value1 < value2 else 0
+  return result, pointer + 4
+
+def intEquals(stack, pointer, mode):
+  result = stack.copy()
+
+  value1, value2 = None, None
+  if mode[0] == 0:
+    value1 = result[result[pointer+1]]
+  elif mode[0] == 1:
+    value1 = result[pointer+1]
+
+  if mode[1] == 0:
+    value2 = result[result[pointer+2]]
+  elif mode[1] == 1:
+    value2 = result[pointer+2]
+
+  result[result[pointer+3]] = 1 if value1 == value2 else 0
+
+  return result, pointer + 4
+
+operations = [
+  None,
+  intAdd,
+  intMultiply,
+  intInput,
+  intOutput,
+  intJumpIfTrue,
+  intJumpIfFalse,
+  intLessthan,
+  intEquals
+]
 
 stack = []
 with open('input.txt', 'r') as f:
